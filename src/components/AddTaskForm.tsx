589176a492d66c4fdcc5a2b10f0fc3f54f386eb1
Nick Jacob <njacob341@gmail.com>
2022-07-24 16:20:40 -0700

    Breaking out components: TableForm and AddTaskForm

---
+++ src/components/AddTaskForm.tsx
@@ -0,0 +1,85 @@
+import React, { RefObject, ChangeEvent } from 'react';
+
+import ButtonGradient from './UI/Button/ButtonGradient';
+import classes from '../App.module.scss';
+
+interface Props {
+	handleAddFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+	handleAddFormChange: (
+		e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+	) => void;
+	handleAddFormKeydown: (e: React.KeyboardEvent) => void;
+	isError: boolean;
+	addFormData: AddFormData;
+	priorityInput: RefObject<HTMLInputElement>;
+}
+
+type AddFormData = {
+	status: string | null;
+	letterPriority: string;
+	numberPriority: string;
+	priority: string | null;
+	description: string | null;
+};
+
+const AddTaskForm = ({
+	handleAddFormSubmit,
+	handleAddFormChange,
+	handleAddFormKeydown,
+	addFormData,
+	priorityInput,
+}: Props) => {
+	return (
+		<div className={classes.addTask}>
+			<form onSubmit={handleAddFormSubmit}>
+				<fieldset>
+					<legend>Add a Task</legend>
+					<select
+						onChange={handleAddFormChange}
+						onKeyDown={(e) => handleAddFormKeydown(e)}
+						name='status'
+						value={addFormData.status as string}
+						data-id='status-input'
+						aria-label='Select status'
+					>
+						<option hidden>Select Status</option>
+						<option disabled defaultValue=''>
+							Select Status
+						</option>
+						<option value='In Process'>In Process</option>
+						<option value='Completed'>Completed</option>
+						<option value='Forwarded'>Forwarded</option>
+						<option value='Delegated'>Delegated</option>
+					</select>
+					<input
+						type='text'
+						name='priority'
+						data-id='priority-input'
+						placeholder='ABC'
+						value={addFormData.priority as string}
+						onChange={(e) => handleAddFormChange(e)}
+						onKeyDown={(e) => handleAddFormKeydown(e)}
+						aria-label='Enter task priority'
+						ref={priorityInput}
+					></input>
+					<input
+						type='text'
+						name='description'
+						data-id='description-input'
+						placeholder='Enter task description...'
+						value={addFormData.description as string}
+						onChange={handleAddFormChange}
+						onKeyDown={(e) => handleAddFormKeydown(e)}
+						aria-label='Enter task description'
+						maxLength={150}
+					/>
+					<ButtonGradient type='submit'>
+						<span>Add Task</span>
+					</ButtonGradient>
+				</fieldset>
+			</form>
+		</div>
+	);
+};
+
+export default AddTaskForm;
