40e230b961e14f794c8ef7b3125008bb3aa6c169
Nick Jacob <njacob341@gmail.com>
2022-09-12 14:07:44 -0700

    Removing EditablePriority

--- src/components/TableForm.tsx
+++ src/components/TableForm.tsx
@@ -1,6 +1,5 @@
 import React, { ChangeEvent, RefObject } from 'react';
 
-import EditablePriority from './Cells/EditablePriority';
 import EditableDescription from './Cells/EditableDescription';
 import ReadOnlyStatus from './Cells/ReadOnlyStatus';
 import ReadOnlyPriority from './Cells/ReadOnlyPriority';
@@ -72,21 +71,7 @@ const TableForm = ({
 								handleEditTask={handleEditTask}
 								editTask={editTask}
 							/>
-							{editTask.inputType === 'priority-cell' &&
-							editTask.rowId === task.id ? (
-								<EditablePriority
-									taskPriority={editFormData.priority}
-									handleEditFormChange={handleEditFormChange}
-									handleEditFormSubmit={handleEditFormSubmit}
-									handleEditFormKeyboard={handleEditFormKeyboard}
-									taskId={task.id}
-									isError={isError}
-									rowId={editTask.rowId}
-									inputType={editTask.inputType}
-								/>
-							) : (
-								<ReadOnlyPriority task={task} handleEditTask={handleEditTask} />
-							)}
+							<ReadOnlyPriority task={task} handleEditTask={handleEditTask} />
 							{editTask.inputType === 'description-cell' &&
 							editTask.rowId === task.id ? (
 								<EditableDescription
