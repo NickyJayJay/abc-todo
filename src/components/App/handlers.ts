6e2f87f0555d906796bca040d0cd8ac6f9749610
Nick Jacob <njacob341@gmail.com>
2023-03-06 21:42:08 -0800

    Creating a handlers js file for App component

---
+++ src/components/App/handlers.ts
@@ -0,0 +1,46 @@
+import { Database, DatabaseReference } from 'firebase/database';
+import React from 'react';
+
+import { TaskActionType } from '../../ts/enums';
+import { EditFormData, EditTask } from '../../ts/interfaces';
+import { Task, TaskActionShape } from '../../ts/types';
+
+export interface Options {
+    editTask: EditTask;
+    editFormData: EditFormData;
+    tasks: Task[];
+    taskDispatch: React.Dispatch<TaskActionShape>;
+    sortList: (a: Task[]) => void;
+    ref: (a: Database, b: string) => DatabaseReference;
+    db: Database;
+    update: (a: DatabaseReference, b: object) => Promise<void>;
+}
+
+export const handleFormSubmit = (options: Options) => {
+    const { editTask, editFormData, tasks, taskDispatch, sortList, ref, db, update }: Options = options;
+
+    return (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const editedTask = {
+            id: editTask.rowId,
+            status: editFormData!.status,
+            priority: editFormData!.priority,
+            description: editFormData!.description,
+        };
+
+        const newTasks = [...tasks];
+        const index = tasks.findIndex((task) => task.id === editTask.rowId);
+        newTasks[index] = editedTask;
+
+        taskDispatch({
+            type: TaskActionType.SET,
+            data: newTasks,
+        });
+
+        sortList(newTasks);
+
+        const dbRef = ref(db, `tasks/${editTask.rowId}`);
+        update(dbRef, editedTask);
+    };
+};
