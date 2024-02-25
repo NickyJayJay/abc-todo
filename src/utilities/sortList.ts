139db4406d7881533d8013aa53e22b40e2bd7462
Nick Jacob <njacob341@gmail.com>
2024-02-24 19:05:34 -0800

    Updating sorting logic and adding comments for clarification

--- src/utilities/sortList.ts
+++ src/utilities/sortList.ts
@@ -11,24 +11,31 @@ const sortList = (loadedTasks: Task[]) => {
     const priorityNumberA = Number(numA);
     const priorityNumberB = Number(numB);
 
+    // 'Completed' at very bottom of list
     if (statusA === 'Completed' && statusB !== 'Completed') return 1;
     if (statusA !== 'Completed' && statusB === 'Completed') return -1;
 
+    // 'Forwarded' second from bottom of list
     if (statusA === 'Forwarded' && statusB !== 'Forwarded') return 1;
     if (statusA !== 'Forwarded' && statusB === 'Forwarded') return -1;
 
+    // no priority and no status third from bottom of list
     if (priorityA === '' && priorityB !== '') return 1;
     if (priorityB === '' && priorityA !== '') return -1;
 
-    if (statusA === 'In Progress' && statusB !== 'In Progress') return 1;
-    if (statusA !== 'In Progress' && statusB === 'In Progress') return -1;
+    // status without priority fourth from bottom of list
+    if (statusA === '' && statusB !== '') return 1;
+    if (statusB === '' && statusA !== '') return -1;
 
+    // priority letter sorts in ascending order
     if (letterA > letterB) return 1;
     if (letterA < letterB) return -1;
 
+    // priority number sorts in ascending order
     if (priorityNumberA > priorityNumberB) return 1;
     if (priorityNumberA < priorityNumberB) return -1;
 
+    // priority letter/number combinations appear above letters without number
     if (priorityA.length === 1 && priorityB.length !== 1) return 1;
     if (priorityB.length === 1 && priorityA.length !== 1) return -1;
 
