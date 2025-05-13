f8f75cebe9b284a0c5ea4f503d86940c79656145
Nick Jacob <njacob341@gmail.com>
2025-05-12 18:01:56 -0700

    Initial config v3

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
@@ -7,14 +8,22 @@ const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
-app.use(express.static('.client/public'));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.json());
 
-app.get('/*name', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
+//routes
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-});
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
