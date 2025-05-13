64f44598666d9049ce795334044e1d11d9edf7a6
Nick Jacob <njacob341@gmail.com>
2025-05-12 17:55:43 -0700

    Initial config part 2

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.static(path.join(__dirname, 'build')));
+// middleware
+app.use(express.static('.client/public'));
+app.use(express.json());
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+app.get('/*name', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}...`);
+    console.log(`Server is listening on port ${port}...`);
 });
--- src/ts/enums.ts
+++
@@ -1,5 +0,0 @@
-export enum TaskActionType {
-	SET,
-	ADD,
-	REMOVE,
-}
